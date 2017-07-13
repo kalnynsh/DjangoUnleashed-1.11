63c210e33a3e181abf332198f7d9590d504cd060
ddd <kda869@yandex.ru>
2017-07-13 20:02:45 +0300

    In class Post add class Meta.

--- blog/models.py
+++ blog/models.py
@@ -12,3 +12,8 @@ class Post(models.Model):
 
     def __str__(self):
         return "{} on {}".format(self.title, self.pub_date.strftime('%Y-%m-%d'))
+
+    class Meta:
+        verbose_name = 'blog post'
+        ordering = ['-pub_date', 'title']
+        get_latest_by = 'pub_date'
