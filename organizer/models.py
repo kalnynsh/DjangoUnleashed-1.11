d11b9418c23cc9d2ea2920ec3a53c4cddafcee96
ddd <kda869@yandex.ru>
2017-07-13 18:34:57 +0300

    In classes add __str__ methods.

--- organizer/models.py
+++ organizer/models.py
@@ -5,6 +5,9 @@ class Tag(models.Model):
     name = models.CharField(max_length=31, unique=True)
     slug = models.SlugField(unique=True, help_text='A label for URL config.')
 
+    def __str__(self):
+        return self.name
+
 
 class Startup(models.Model):
     name = models.CharField(max_length=31, db_index=True)
@@ -15,9 +18,15 @@ class Startup(models.Model):
     website = models.URLField(max_length=255)
     tags = models.ManyToManyField(Tag)
 
+    def __str__(self):
+        return self.name
+
 
 class NewsLink(models.Model):
     title = models.CharField(max_length=63)
     pub_date = models.DateField('date published')
     link = models.URLField(max_length=255)
     startup = models.ForeignKey(Startup)
+
+    def __str__(self):
+        return "{}:{}".format(self.startup, self.title)
