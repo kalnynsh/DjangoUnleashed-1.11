343b2daea451f7a85fa48154b0c68cef3983a0e3
ddd <kda869@yandex.ru>
2017-07-19 18:34:48 +0300

    Refactor code with loader, context, render.

--- organizer/views.py
+++ organizer/views.py
@@ -1,3 +1,4 @@
+from django.template import Context, loader
 from django.http.response import HttpResponse
 
 from .models import Tag
@@ -5,21 +6,8 @@ from .models import Tag
 
 def homepage(request):
     tag_list = Tag.objects.all()
-    html_output = "<html>\n"
-    html_output += "<head>\n"
-    html_output += "<title>"
-    html_output += "Don't Do This!</title>\n"
-    html_output += "</head>\n"
-    html_output += "<body>\n"
+    template = loader.get_template('organizer/tag_list.html')
+    context = Context({'tag_list': tag_list})
+    output = template.render(context)
 
-    html_output += "<ul>\n"
-    for tag in tag_list:
-        html_output += "<li>\n"
-        html_output += tag.name.title()
-        html_output += "</li>\n"
-    html_output += "</ul>\n"
-
-    html_output += "</body>\n"
-    html_output += "</html>\n"
-
-    return HttpResponse(html_output)
+    return HttpResponse(output)
