01efaff387021e21b5aa8716daebaa2134c9422f
ddd <kda869@yandex.ru>
2017-07-19 19:21:08 +0300

    Revise code the context part using dict.

--- organizer/views.py
+++ organizer/views.py
@@ -7,7 +7,7 @@ from .models import Tag
 def homepage(request):
     tag_list = Tag.objects.all()
     template = loader.get_template('organizer/tag_list.html')
-    context = Context({'tag_list': tag_list})
+    context = {'tag_list': tag_list, }
     output = template.render(context)
 
     return HttpResponse(output)
