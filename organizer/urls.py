bde6bc281fb4407250593f60d8688494dfae50b4
ddd <kda869@yandex.ru>
2017-07-23 19:14:26 +0300

    Add url name='organizer_startup_list'.

--- organizer/urls.py
+++ organizer/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 
-from .views import homepage, tag_detail, tag_list
+from .views import homepage, tag_detail, tag_list, startup_list
 
 
 urlpatterns = [
     url(r'^$', homepage),
     url(r'^tag/$', tag_list, name='organizer_tag_list'),
     url(r'^tag/(?P<slug>[\w\-]+)/$', tag_detail, name='organizer_tag_detail'),
+    url(r'^startup/$', startup_list, name='organizer_startup_list')
 ]
