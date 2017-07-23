92340850dc78a241dab6b086faee2930f0958758
ddd <kda869@yandex.ru>
2017-07-23 19:48:08 +0300

    Create def post_list(request).

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+
+def post_list(request):
+    post_list_obj = Post.objects.all()
+    template = 'blog/post_list.html'
+    context = {'post_list': post_list_obj}
+
+    return render(
+        request,
+        template,
+        context,
+    )
