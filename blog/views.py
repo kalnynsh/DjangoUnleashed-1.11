b042649a9077c823feb1fccc77ff35186c6ae093
ddd <kda869@yandex.ru>
2017-07-23 20:26:50 +0300

    Add def post_detail().

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Post
 
@@ -7,6 +7,19 @@ def post_list(request):
     post_list_obj = Post.objects.all()
     template = 'blog/post_list.html'
     context = {'post_list': post_list_obj}
+
+    return render(
+        request,
+        template,
+        context,
+    )
+
+
+def post_detail(request, year, month, slug):
+    post_detail_obj = \
+        Post.objects.filter(pub__date__year=year).filter(pub__date__month=month).get(slug__iexact=slug)
+    template = 'blog/post_detail.html'
+    context = {'post_detail': post_detail_obj}
 
     return render(
         request,
