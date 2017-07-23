36edf8121e61b07e3a34f983e09c1064c14339b8
ddd <kda869@yandex.ru>
2017-07-23 18:43:57 +0300

    Refactor ulrpatterns with include(organizer_urls).

--- suorganizer/urls.py
+++ suorganizer/urls.py
@@ -13,14 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-from organizer.views import homepage
-from organizer.views import tag_detail
+from organizer import urls as organizer_urls
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', homepage),
-    url(r'^tag/(?P<slug>[\w\-]+)/$', tag_detail, name='organizer_tag_detail'),
+    url(r'^', include(organizer_urls)),
 ]
