2f5e26f0aea10918fc5e249a6aecb6069b5f5593
ddd <kda869@yandex.ru>
2017-07-23 18:49:07 +0300

    Edit error not closed group.

--- organizer/urls.py
+++ organizer/urls.py
@@ -20,5 +20,5 @@ from .views import homepage, tag_detail
 
 urlpatterns = [
     url(r'^$', homepage),
-    url(r'^tag/(?P<slug>[\w\-]+/$', tag_detail, name='organizer_tag_detail'),
+    url(r'^tag/(?P<slug>[\w\-]+)/$', tag_detail, name='organizer_tag_detail'),
 ]
