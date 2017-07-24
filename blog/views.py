6e3b397898f487c16701c0e5aacbe0360abadd1f
ddd <kda869@yandex.ru>
2017-07-24 18:27:04 +0300

    In def post_detail add in context dict pare 'parent_template': parent_template.

--- blog/views.py
+++ blog/views.py
@@ -18,14 +18,14 @@ class PostListView(View):
         )
 
 
-def post_detail(request, year, month, slug):
+def post_detail(request, year, month, slug, parent_template=None):
     post_detail_obj = get_object_or_404(
         Post,
         pub__date__year=year,
         pub__date__month=month,
         slug__iexact=slug)
     template = 'blog/post_detail.html'
-    context = {'post': post_detail_obj}
+    context = {'post': post_detail_obj, 'parent_template': parent_template, }
 
     return render(
         request,
