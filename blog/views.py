66449da4ca9a8f9fbaec51ddeca3d3e70abe8047
ddd <kda869@yandex.ru>
2017-07-24 17:49:46 +0300

    Refactor def post_detail() with post_detail_obj = get_object_or_404.

--- blog/views.py
+++ blog/views.py
@@ -16,8 +16,11 @@ def post_list(request):
 
 
 def post_detail(request, year, month, slug):
-    post_detail_obj = \
-        Post.objects.filter(pub__date__year=year).filter(pub__date__month=month).get(slug__iexact=slug)
+    post_detail_obj = get_object_or_404(
+        Post,
+        pub__date__year=year,
+        pub__date__month=month,
+        slug__iexact=slug)
     template = 'blog/post_detail.html'
     context = {'post_detail': post_detail_obj}
 
