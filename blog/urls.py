f7b0c3179918f0617fac80da2179053f866f7d79
ddd <kda869@yandex.ru>
2017-07-24 18:15:54 +0300

    Refactor with PostListView.as_view().

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
 
-from .views import post_detail, post_list
+from .views import post_detail, PostListView
 
 
 urlpatterns = [
-    url(r'^$', post_list, name='blog_post_list'),
+    url(r'^$', PostListView.as_view(), name='blog_post_list'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<slug>[\w\-]+)/$',
         post_detail, name='blog_post_detail'),
 ]
