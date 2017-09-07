078e66dcc10c07550ed6bb8af7cf2d0c09f06528
ddd <kda869@yandex.ru>
2017-09-07 19:27:36 +0300

    Change url(r'^startup_list/$',..) to r'^startup/$',..

--- organizer/urls.py
+++ organizer/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     url(r'^tag/$', tag_list, name='organizer_tag_list'),
     url(r'^tag/create/$', tag_create, name='organizer_tag_create'),
     url(r'^tag/(?P<slug>[\w\-]+)/$', tag_detail, name='organizer_tag_detail'),
-    url(r'^startup_list/$', startup_list, name='organizer_startup_list'),
+    url(r'^startup/$', startup_list, name='organizer_startup_list'),
     url(r'^startup/(?P<slug>[\w\-]+)/$', startup_detail, name='organizer_startup_detail'),
 ]
