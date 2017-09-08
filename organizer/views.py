01b2bacd4939914de1b9b773521738d6d54bf08d
ddd <kda869@yandex.ru>
2017-09-08 20:08:54 +0300

    Create class TagCreate(View) and replace tag_create(). Change def() in file.

--- organizer/views.py
+++ organizer/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404, render, redirect
+from django.views.generic import View
 
 from .models import Tag, Startup
 from .forms import TagForm
@@ -16,6 +17,47 @@ def homepage(request):
     )
 
 
+def startup_detail(request, slug):
+    startup = get_object_or_404(Startup, slug__iexact=slug)
+    template = 'organizer/startup_detail.html'
+    context = {'startup': startup}
+
+    return render(
+        request,
+        template,
+        context
+    )
+
+
+def startup_list(request):
+    startup_list_obj = Startup.objects.all()
+    template = 'organizer/startup_list.html'
+    context = {'startup_list': startup_list_obj}
+
+    return render(
+        request,
+        template,
+        context
+    )
+
+
+class TagCreate(View):
+    form_class = TagForm
+    template_name = 'organizer/tag_form.html'
+
+    def get(self, request):
+        context = {'form': self.form_class()}
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        bound_form = self.form_class(request.POST)
+        if bound_form.is_valid():
+            new_tag = bound_form.save()
+            return redirect(new_tag)
+        else:
+            return render(request, self.template_name, {'form': bound_form})
+
+
 def tag_detail(request, slug):
     tag = get_object_or_404(Tag, slug__iexact=slug)
     template = 'organizer/tag_detail.html'
@@ -31,54 +73,6 @@ def tag_detail(request, slug):
 def tag_list(request):
     template = 'organizer/tag_list.html'
     context = {'tag_list': Tag.objects.all()}
-
-    return render(
-        request,
-        template,
-        context
-    )
-
-
-def tag_create(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = TagForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            new_tag = form.save()
-            # Return new URL
-            # tag_url = new_tag.get_absolute_url()
-            #  HttpResponseRedirect(tag_url)
-            return redirect(new_tag)
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = TagForm()
-
-    template = 'organizer/tag_form.html'
-    context = {'form': form}
-
-    return render(request, template, context)
-
-
-def startup_list(request):
-    startup_list_obj = Startup.objects.all()
-    template = 'organizer/startup_list.html'
-    context = {'startup_list': startup_list_obj}
-
-    return render(
-        request,
-        template,
-        context
-    )
-
-
-def startup_detail(request, slug):
-    startup = get_object_or_404(Startup, slug__iexact=slug)
-    template = 'organizer/startup_detail.html'
-    context = {'startup': startup}
 
     return render(
         request,
