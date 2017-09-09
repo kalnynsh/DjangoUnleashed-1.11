ad0fbb4e741555f44bbf9e9683364d2f00b9f975
ddd <kda869@yandex.ru>
2017-09-09 19:26:28 +0300

    Add class PostCreate(View), decorator @require_http_methods(['HEAD', 'GET']) def post_detail(..).

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,42 @@
+from django.shortcuts import get_object_or_404, render, redirect
+from django.views.decorators.http import require_http_methods
 from django.views.generic import View
-from django.shortcuts import get_object_or_404, render
 
 from .models import Post
+from .forms import PostForm
+
+
+class PostCreate(View):
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def get(self, request):
+        return render(request, self.template_name, {'form': self.form_class()})
+
+    def post(self, request):
+        bound_form = self.form_class(request.POST)
+        if bound_form.is_valid():
+            new_post = bound_form.save()
+            return redirect(new_post)
+        else:
+            return render(request, self.template_name, {'form': bound_form})
+
+
+@require_http_methods(['HEAD', 'GET'])
+def post_detail(request, year, month, slug, parent_template=None):
+    post_detail_obj = get_object_or_404(
+        Post,
+        pub__date__year=year,
+        pub__date__month=month,
+        slug__iexact=slug)
+    template = 'blog/post_detail.html'
+    context = {'post': post_detail_obj, 'parent_template': parent_template, }
+
+    return render(
+        request,
+        template,
+        context,
+    )
 
 
 class PostListView(View):
@@ -17,19 +52,3 @@ class PostListView(View):
             self.template_name,
             context
         )
-
-
-def post_detail(request, year, month, slug, parent_template=None):
-    post_detail_obj = get_object_or_404(
-        Post,
-        pub__date__year=year,
-        pub__date__month=month,
-        slug__iexact=slug)
-    template = 'blog/post_detail.html'
-    context = {'post': post_detail_obj, 'parent_template': parent_template, }
-
-    return render(
-        request,
-        template,
-        context,
-    )
