dc29443d01a480d94270f4602a12beb3358ddb7b
ddd <kda869@yandex.ru>
2017-09-09 19:35:23 +0300

    Add url(r'^create/$', PostCreateView.as_view(), name='blog_post_create').

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import url
 
-from .views import post_detail, PostListView
+from .views import PostCreateView, post_detail, PostListView
 
 
 urlpatterns = [
-    url(r'^$', PostListView.as_view(),
-        {'parent_template': 'base.html'},
-        name='blog_post_list'),
+    url(r'^$', PostListView.as_view(), {'parent_template': 'base.html'}, name='blog_post_list'),
+    url(r'^create/$', PostCreateView.as_view(), name='blog_post_create'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<slug>[\w\-]+)/$',
         post_detail,
         {'parent_template': 'base.html'},
