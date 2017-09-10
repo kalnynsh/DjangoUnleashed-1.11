c0c8e87e98f87265acf8a6bb7101cd508bb5d850
ddd <kda869@yandex.ru>
2017-09-10 20:11:25 +0300

    In post_detail edit pub_date__year|month.

--- blog/views.py
+++ blog/views.py
@@ -26,8 +26,8 @@ class PostCreateView(View):
 def post_detail(request, year, month, slug, parent_template=None):
     post_detail_obj = get_object_or_404(
         Post,
-        pub__date__year=year,
-        pub__date__month=month,
+        pub_date__year=year,
+        pub_date__month=month,
         slug__iexact=slug)
     template = 'blog/post_detail.html'
     context = {'post': post_detail_obj, 'parent_template': parent_template, }
