ca939b8ae4f3aa2e0b514e150278811ca9e345ed
ddd <kda869@yandex.ru>
2017-09-11 18:25:52 +0300

    Inherit from class ObjectCreateMixin in classes NewsLinkCreateView, StartupCreateView, TagCreateView.

--- organizer/views.py
+++ organizer/views.py
@@ -1,40 +1,19 @@
-from django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import get_object_or_404, render
 from django.views.generic import View
 
 from .models import Tag, Startup
 from .forms import TagForm, StartupForm, NewsLinkForm
+from .utils import ObjectCreateMixin
 
 
-class NewsLinkCreateView(View):
+class NewsLinkCreateView(ObjectCreateMixin, View):
     form_class = NewsLinkForm
     template_name = 'organizer/newslink_form.html'
 
-    def get(self, request):
-        return render(request, self.template_name, {'form': self.form_class()})
 
-    def post(self, request):
-        bound_form = self.form_class(request.POST)
-        if bound_form.is_valid():
-            new_newslink = bound_form.save()
-            return redirect(new_newslink)
-        else:
-            return render(request, self.template_name, {'form': bound_form})
-
-
-class StartupCreateView(View):
+class StartupCreateView(ObjectCreateMixin, View):
     form_class = StartupForm
     template_name = 'organizer/startup_form.html'
-
-    def get(self, request):
-        return render(request, self.template_name, {'form': self.form_class()})
-
-    def post(self, request):
-        bound_form = self.form_class(request.POST)
-        if bound_form.is_valid():
-            new_startup = bound_form.save()
-            return redirect(new_startup)
-        else:
-            return render(request, self.template_name, {'form': bound_form})
 
 
 def startup_detail(request, slug):
@@ -61,21 +40,9 @@ def startup_list(request):
     )
 
 
-class TagCreateView(View):
+class TagCreateView(ObjectCreateMixin, View):
     form_class = TagForm
     template_name = 'organizer/tag_form.html'
-
-    def get(self, request):
-        context = {'form': self.form_class()}
-        return render(request, self.template_name, context)
-
-    def post(self, request):
-        bound_form = self.form_class(request.POST)
-        if bound_form.is_valid():
-            new_tag = bound_form.save()
-            return redirect(new_tag)
-        else:
-            return render(request, self.template_name, {'form': bound_form})
 
 
 def tag_detail(request, slug):
