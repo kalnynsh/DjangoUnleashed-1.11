65797c54a2f0ab118a7c9f809a6e5a4574c667ed
ddd <kda869@yandex.ru>
2017-09-12 20:39:58 +0300

    Update code.

--- organizer/forms.py
+++ organizer/forms.py
@@ -4,6 +4,12 @@ from django.core.exceptions import ValidationError
 from .models import NewsLink, Startup, Tag
 
 
+class NewsLinkForm(forms.ModelForm):
+    class Meta:
+        model = NewsLink
+        fields = '__all__'
+
+
 class SlugCleanMixin:
     """Mixin class for slug cleaning method."""
     def clean_slug(self):
@@ -14,6 +20,12 @@ class SlugCleanMixin:
         return new_slug
 
 
+class StartupForm(SlugCleanMixin, forms.ModelForm):
+    class Meta:
+        model = Startup
+        fields = '__all__'
+
+
 class TagForm(SlugCleanMixin, forms.ModelForm):
     class Meta:
         model = Tag
@@ -21,15 +33,3 @@ class TagForm(SlugCleanMixin, forms.ModelForm):
 
     def clean_name(self):
         return self.cleaned_data['name'].lower()
-
-
-class NewsLinkForm(forms.ModelForm):
-    class Meta:
-        model = NewsLink
-        fields = '__all__'
-
-
-class StartupForm(SlugCleanMixin, forms.ModelForm):
-    class Meta:
-        model = Startup
-        fields = '__all__'
