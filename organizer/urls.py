b032bc83e9c3332f368d6aae61fbff0ff23a4cd7
ddd <kda869@yandex.ru>
2017-09-12 19:37:19 +0300

    Add url(r'^startup/(?P<slug>[\w\-]+)/update/$', StartupUpdateView.as_view(), ..

--- organizer/urls.py
+++ organizer/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url
 
 from .views import (NewsLinkCreateView, NewsLinkUpdateView,
                     TagCreateView, TagUpdateView, tag_detail, tag_list,
-                    StartupCreateView, startup_list, startup_detail,
+                    StartupCreateView, startup_list, startup_detail, StartupUpdateView,
                     )
 
 
@@ -27,6 +27,7 @@ urlpatterns = [
     url(r'^startup/$', startup_list, name='organizer_startup_list'),
     url(r'^startup/create/$', StartupCreateView.as_view(), name='organizer_startup_create'),
     url(r'^startup/(?P<slug>[\w\-]+)/$', startup_detail, name='organizer_startup_detail'),
+    url(r'^startup/(?P<slug>[\w\-]+)/update/$', StartupUpdateView.as_view(), name='organizer_startup_update'),
     url(r'^tag/$', tag_list, name='organizer_tag_list'),
     url(r'^tag/create/$', TagCreateView.as_view(), name='organizer_tag_create'),
     url(r'^tag/(?P<slug>[\w\-]+)/$', tag_detail, name='organizer_tag_detail'),
