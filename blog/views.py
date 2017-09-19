39c5ee410f99ea2524f6fbfe71360d06a53e1862
kalnynsh <kda869@yandex.ru>
2017-09-19 20:41:30 +0300

    Update code, edit mistake sug -> slug.

--- blog/views.py
+++ blog/views.py
@@ -89,12 +89,13 @@ class PostUpdateView(View):
 
 
 class PostDeleteView(View):
+
     def get(self, request, year, month, slug):
         post = get_object_or_404(
             Post,
             pub_date__year=year,
             pub_date__month=month,
-            sug__iexact=slug
+            slug__iexact=slug
         )
         return render(request,
                       'blog/post_confirm_delete.html',
@@ -105,7 +106,7 @@ class PostDeleteView(View):
             Post,
             pub_date__year=year,
             pub_date__month=month,
-            sug__iexact=slug
+            slug__iexact=slug
         )
         post.delete()
         return redirect('blog_post_list')
