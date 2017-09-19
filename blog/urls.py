22c482cdf44b83a52aa252fdd9b1d9af615ab689
kalnynsh <kda869@yandex.ru>
2017-09-19 17:14:30 +0300

    Add url(.., name='blog_post_delete').

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from .views import PostCreateView, post_detail, PostListView, PostUpdateView
+from .views import PostCreateView, post_detail, PostListView, PostUpdateView, PostDeleteView
 
 
 urlpatterns = [
@@ -10,6 +10,9 @@ urlpatterns = [
         post_detail,
         {'parent_template': 'base.html'},
         name='blog_post_detail'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<slug>[\w\-]+)/delete/$',
+        PostDeleteView.as_view(),
+        name='blog_post_delete'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<slug>[\w\-]+)/update/$',
         PostUpdateView.as_view(), name='blog_post_update'),
 ]
