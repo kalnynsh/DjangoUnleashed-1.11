244dbc723f3a6730645d84ce3c4e071fe0e797e1
kalnynsh <kda869@yandex.ru>
2017-09-25 18:29:11 +0300

    Add url(r'^contact/', include(contact_urls)).

--- suorganizer/urls.py
+++ suorganizer/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from organizer import urls as organizer_urls
 from blog import urls as blog_urls
 from .views import redirect_root
+from contact import urls as contact_urls
 
 
 urlpatterns = [
@@ -25,4 +26,5 @@ urlpatterns = [
     url(r'^$', redirect_root),
     url(r'^', include(organizer_urls)),
     url(r'^blog/', include(blog_urls)),
+    url(r'^contact/', include(contact_urls)),
 ]
