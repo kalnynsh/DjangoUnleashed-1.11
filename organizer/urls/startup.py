035f683574c45189107b668c1ae020bf9a9d5b57
kalnynsh <kda869@yandex.ru>
2017-09-26 17:53:07 +0300

    Split organizer ulrs, one part is 'startup'.

---
+++ organizer/urls/startup.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+
+from ..views import (StartupCreateView, StartupDeleteView,
+                     StartupUpdateView, startup_detail, startup_list)
+
+urlpatterns = [
+    url(r'^$', startup_list,
+        name='organizer_startup_list'),
+    url(r'^create/$', StartupCreateView.as_view(),
+        name='organizer_startup_create'),
+    url(r'^(?P<slug>[\w\-]+)/$', startup_detail,
+        name='organizer_startup_detail'),
+    url(r'^(?P<slug>[\w\-]+)/delete/$', StartupDeleteView.as_view(),
+        name='organizer_startup_delete'),
+    url(r'^(?P<slug>[\w\-]+)/update/$', StartupUpdateView.as_view(),
+        name='organizer_startup_update'),
+]
