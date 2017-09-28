0501e84684cafa527316bb8881dcdd44ab5bc213
kalnynsh <kda869@yandex.ru>
2017-09-28 19:45:50 +0300

    In class StartupListView add  page_kwarg = 'page', page_number = request.GET.get(self.page_kwarg).

--- organizer/views.py
+++ organizer/views.py
@@ -79,17 +79,19 @@ def startup_detail(request, slug):
 
 
 class StartupListView(View):
+    page_kwarg = 'page'
     paginate_by = 5  # 5 items per page
     template_name = 'organizer/startup_list.html'
 
     def get(self, request):
         startups = Startup.objects.all()
         paginator = Paginator(startups, self.paginate_by)
-        page = paginator.page(1)
+        page_number = request.GET.get(self.page_kwarg)
+        page = paginator.page(page_number)
         context = {
             'is_paginated': page.has_other_pages(),
             'paginator': paginator,
-            'startup_list': page
+            'startup_list': page,
         }
 
         return render(
