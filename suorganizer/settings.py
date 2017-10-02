b22ec4cbceafe6c6e02f762feb00cfe6c6f269ba
kalnynsh <kda869@yandex.ru>
2017-10-02 20:25:35 +0300

    Update code.

--- suorganizer/settings.py
+++ suorganizer/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.middleware.FlatpageFallbackMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 ]
 
 ROOT_URLCONF = 'suorganizer.urls'
