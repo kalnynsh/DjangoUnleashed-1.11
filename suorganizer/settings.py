cdd1bb6b7c6610dc6c377e15d6d4681a46de059d
kalnynsh <kda869@yandex.ru>
2017-10-02 20:22:18 +0300

    Add to MIDDLEWARE [ .., 'django.contrib.middleware.FlatpageFallbackMiddleware',]

--- suorganizer/settings.py
+++ suorganizer/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.middleware.FlatpageFallbackMiddleware',
 ]
 
 ROOT_URLCONF = 'suorganizer.urls'
