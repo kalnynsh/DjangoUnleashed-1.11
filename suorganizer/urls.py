767d3967beac6daac5017f5d0f0472eedee01eb4
kalnynsh <kda869@yandex.ru>
2017-10-11 19:32:09 +0300

    Refactor with url(r'^about/$', TemplateView.as_view(template_name="site/about.html"), name='about_site'), using it in header_navbar.html.

--- suorganizer/urls.py
+++ suorganizer/urls.py
@@ -19,18 +19,19 @@ from organizer.urls import (
     newslink as newslink_urls, startup as startup_urls,
     tag as tag_urls)
 from blog import urls as blog_urls
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, TemplateView
 from contact import urls as contact_urls
 from django.contrib.flatpages import urls as flatpages_urls
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+
     url(r'^$', RedirectView.as_view(pattern_name='blog_post_list', permanent=False)),
+    url(r'^about/$', TemplateView.as_view(template_name="site/about.html"), name='about_site'),
+    url(r'^admin/', admin.site.urls),
+    url(r'^blog/', include(blog_urls)),
+    url(r'^contact/', include(contact_urls)),
     url(r'^newslink/', include(newslink_urls)),
     url(r'^startup/', include(startup_urls)),
     url(r'^tag/', include(tag_urls)),
-    url(r'^blog/', include(blog_urls)),
-    url(r'^contact/', include(contact_urls)),
-    url(r'^', include(flatpages_urls)),
 ]
