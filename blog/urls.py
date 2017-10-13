b1069bc450b662ea34b2962fd00b5c2d528d9725
kalnynsh <kda869@yandex.ru>
2017-10-13 17:11:04 +0300

    Add url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$', PostArchiveMonthView.as_view(), name='blog_post_archive_month').

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import url
 
 from .views import PostCreateView, post_detail, PostListView, PostUpdateView, \
-                    PostDeleteView, PostArchiveYearView
+                    PostDeleteView, PostArchiveMonthView, PostArchiveYearView
 
 
 urlpatterns = [
     url(r'^$', PostListView.as_view(), {'parent_template': 'base.html'}, name='blog_post_list'),
     url(r'^create/$', PostCreateView.as_view(), name='blog_post_create'),
     url(r'^(?P<year>\d{4})/$', PostArchiveYearView.as_view(), name='blog_post_archive_year'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$', PostArchiveMonthView.as_view(), name='blog_post_archive_month'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<slug>[\w\-]+)/$',
         post_detail,
         {'parent_template': 'base.html'},
