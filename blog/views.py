747a95966549d5ed46541c462e3e7aa04592b686
kalnynsh <kda869@yandex.ru>
2017-10-14 18:05:29 +0300

    Refactor class PostListView(ArchiveIndexView).

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,10 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.views.decorators.http import require_http_methods
-from django.views.generic import CreateView, ListView, View, \
-                                 YearArchiveView, MonthArchiveView
+from django.views.generic import CreateView, View, YearArchiveView, \
+                                 MonthArchiveView, ArchiveIndexView
 
 from .models import Post
 from .forms import PostForm
-
-
-class PostCreateView(CreateView):
-    form_class = PostForm
-    model = Post
-    # template_name = 'blog/post_form.html' derive from model name
 
 
 class PostArchiveMonthView(MonthArchiveView):
@@ -23,6 +17,36 @@ class PostArchiveYearView(YearArchiveView):
     model = Post
     date_field = 'pub_date'
     make_object_list = True
+
+
+class PostCreateView(CreateView):
+    form_class = PostForm
+    model = Post
+    # template_name = 'blog/post_form.html' derive from model name
+
+
+class PostDeleteView(View):
+
+    def get(self, request, year, month, slug):
+        post = get_object_or_404(
+            Post,
+            pub_date__year=year,
+            pub_date__month=month,
+            slug__iexact=slug
+        )
+        return render(request,
+                      'blog/post_confirm_delete.html',
+                      {'post': post})
+
+    def post(self, request, year, month, slug):
+        post = get_object_or_404(
+            Post,
+            pub_date__year=year,
+            pub_date__month=month,
+            slug__iexact=slug
+        )
+        post.delete()
+        return redirect('blog_post_list')
 
 
 @require_http_methods(['HEAD', 'GET'])
@@ -42,8 +66,15 @@ def post_detail(request, year, month, slug, parent_template=None):
     )
 
 
-class PostListView(ListView):
+class PostListView(ArchiveIndexView):
+    allow_empty = True
+    allow_future = True
+    context_object_name = 'post_list'
+    date_field = 'pub_date'
+    make_object_list = True
     model = Post
+    paginate_by = 5
+    template_name = 'blog/post_list.html'
 
 
 class PostUpdateView(View):
@@ -79,26 +110,3 @@ class PostUpdateView(View):
             }
             return render(request, self.template_name, context)
 
-
-class PostDeleteView(View):
-
-    def get(self, request, year, month, slug):
-        post = get_object_or_404(
-            Post,
-            pub_date__year=year,
-            pub_date__month=month,
-            slug__iexact=slug
-        )
-        return render(request,
-                      'blog/post_confirm_delete.html',
-                      {'post': post})
-
-    def post(self, request, year, month, slug):
-        post = get_object_or_404(
-            Post,
-            pub_date__year=year,
-            pub_date__month=month,
-            slug__iexact=slug
-        )
-        post.delete()
-        return redirect('blog_post_list')
