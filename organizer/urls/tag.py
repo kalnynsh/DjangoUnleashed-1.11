265be4e0c1d7c47bd93b684aaee0357205d74600
kalnynsh <kda869@yandex.ru>
2017-10-15 20:01:21 +0300

    Remove old.

--- organizer/urls/tag.py
+++ organizer/urls/tag.py
@@ -1,16 +1,13 @@
 from django.conf.urls import url
 
 from ..views import (TagCreateView, TagUpdateView,
-                     TagDetailView, TagListView, TagDeleteView,
-                     TagPageListView)
+                     TagDetailView, TagListView, TagDeleteView)
 
 
 urlpatterns = [
     url(r'^$', TagListView.as_view(), name='organizer_tag_list'),
     url(r'^create/$', TagCreateView.as_view(),
         name='organizer_tag_create'),
-    url(r'^(?P<page_number>\d+)/$', TagPageListView.as_view(),
-        name='organizer_tag_page'),
     url(r'^(?P<slug>[\w\-]+)/$', TagDetailView.as_view(),
         name='organizer_tag_detail'),
     url(r'^(?P<slug>[\w\-]+)/delete/$', TagDeleteView.as_view(),
