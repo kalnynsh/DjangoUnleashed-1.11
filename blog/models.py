8f26560b3d59e1081e47429d176ad102e895985e
kalnynsh <kda869@yandex.ru>
2017-10-16 19:57:39 +0300

    Revise def get_absolute_url() ..'month': self.pub_date.month,..

--- blog/models.py
+++ blog/models.py
@@ -21,7 +21,7 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog_post_detail', kwargs={'year': self.pub_date.year,
-                                                   'month': self.pub_date.strftime('%b').lower(),
+                                                   'month': self.pub_date.month,
                                                    'day': self.pub_date.day,
                                                    'slug': self.slug})
 
