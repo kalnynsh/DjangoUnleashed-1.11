e304be9885878219d7f4ab90672790d33cba0aca
kalnynsh <kda869@yandex.ru>
2017-10-16 20:00:18 +0300

    Remove the day part from url(..name='blog_post_detail').

--- blog/urls.py
+++ blog/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^create/$', PostCreateView.as_view(), name='blog_post_create'),
     url(r'^(?P<year>\d{4})/$', PostArchiveYearView.as_view(), name='blog_post_archive_year'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$', PostArchiveMonthView.as_view(), name='blog_post_archive_month'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>[\w\-]+)/$',
+    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<slug>[\w\-]+)/$',
         PostDetailView.as_view(), name='blog_post_detail'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<slug>[\w\-]+)/delete/$',
         PostDeleteView.as_view(),
