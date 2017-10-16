1e1f2b948a4e7a80c2349cedf79e13b2e916f7e5
kalnynsh <kda869@yandex.ru>
2017-10-16 18:03:46 +0300

    Class PageLinksMixin add methods: first_page(), last_page() - create list on appropriate pages.

--- organizer/utils.py
+++ organizer/utils.py
@@ -4,6 +4,12 @@ class PageLinksMixin:
     def _page_urls(self, page_number):
         return "?{pkw}={n}".format(pkw=self.page_kwarg, n=page_number)
 
+    def first_page(self, page):
+        # don't show on first page
+        if page.number > 1:
+            return self._page_urls(1)
+        return None
+
     def previous_page(self, page):
         if page.has_previous():
             return self._page_urls(page.previous_page_number())
@@ -12,6 +18,12 @@ class PageLinksMixin:
     def next_page(self, page):
         if page.has_next():
             return self._page_urls(page.next_page_number())
+        return None
+
+    def last_page(self, page):
+        last_page = page.paginator.num_pages
+        if page.number < last_page:
+            return self._page_urls(last_page)
         return None
 
     def get_context_data(self, **kwargs):
