5967785c75205c31affa5e5773a049e8658c208a
kalnynsh <kda869@yandex.ru>
2017-10-16 19:42:37 +0300

    Import PostDetailView, add it to urlpatterns.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
-from .views import PostCreateView, post_detail, PostListView, PostUpdateView, \
-                    PostDeleteView, PostArchiveMonthView, PostArchiveYearView
+from .views import PostArchiveMonthView, PostArchiveYearView, PostCreateView, \
+                    PostDeleteView, PostDetailView,  PostListView, PostUpdateView
 
 
 urlpatterns = [
@@ -9,10 +9,8 @@ urlpatterns = [
     url(r'^create/$', PostCreateView.as_view(), name='blog_post_create'),
     url(r'^(?P<year>\d{4})/$', PostArchiveYearView.as_view(), name='blog_post_archive_year'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$', PostArchiveMonthView.as_view(), name='blog_post_archive_month'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<slug>[\w\-]+)/$',
-        post_detail,
-        {'parent_template': 'base.html'},
-        name='blog_post_detail'),
+    url(r'^(?P<year>\d{4})/(?P<month>[\w-]+)/(?P<day>\d{1,2})/(?P<slug>[\w\-]+)/$',
+        PostDetailView.as_view(), name='blog_post_detail'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<slug>[\w\-]+)/delete/$',
         PostDeleteView.as_view(),
         name='blog_post_delete'),
