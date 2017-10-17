00fb3e16f2da2a82f0f0055907e47bd4175e5df5
kalnynsh <kda869@yandex.ru>
2017-10-17 17:56:13 +0300

    Create class PostGetMixin(..) with def get_object(..)..

---
+++ blog/utils.py
@@ -0,0 +1,17 @@
+from django.shortcuts import get_object_or_404
+
+from .models import Post
+
+
+class PostGetMixin:
+
+    def get_object(self, queryset=None):
+        year = self.kwargs.get('year')
+        month = self.kwagrs.get('month')
+        slug = self.kwargs.get('slug')
+        return get_object_or_404(
+            Post,
+            pub_date__year=year,
+            pub_date__month=month,
+            slug__iexact=slug
+        )
