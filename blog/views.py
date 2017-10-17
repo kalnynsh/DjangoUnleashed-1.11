40dd1f5187d4eef018b6e63371007463d7e9100c
kalnynsh <kda869@yandex.ru>
2017-10-17 18:40:54 +0300

    Refactor class PostUpdateView(PostGetMixin, UpdateView): ..

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.views.generic import (ArchiveIndexView, CreateView, DetailView,
-                                  MonthArchiveView, View, YearArchiveView)
+                                  MonthArchiveView, View, YearArchiveView,
+                                  UpdateView)
 
 from .models import Post
 from .forms import PostForm
@@ -63,36 +64,6 @@ class PostListView(ArchiveIndexView):
     template_name = 'blog/post_list.html'
 
 
-class PostUpdateView(View):
+class PostUpdateView(PostGetMixin, UpdateView):
     form_class = PostForm
     model = Post
-    template_name = 'blog/post_form_update.html'
-
-    def get_object(self, year, month, slug):
-        return get_object_or_404(
-            self.model,
-            pub_date__year=year,
-            pub_date__month=month,
-            slug=slug)
-
-    def get(self, request, year, month, slug):
-        post = self.get_object(year, month, slug)
-        context = {
-            'form': self.form_class(instance=post),
-            'post': post,
-        }
-        return render(request, self.template_name, context)
-
-    def post(self, request, year, month, slug):
-        post = self.get_object(year, month, slug)
-        bound_form = self.form_class(request.POST, instance=post)
-        if bound_form.is_valid():
-            new_post = bound_form.save()
-            return redirect(new_post)
-        else:
-            context = {
-                'form': bound_form,
-                'post': post,
-            }
-            return render(request, self.template_name, context)
-
